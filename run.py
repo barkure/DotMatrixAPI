ddc80d8ddac33750ab7a5a05011caf4156059179
barkure <43804451+barkure@users.noreply.github.com>
2024-08-05 00:05:03 +0800

    refactor: optimize get_bytes function for better performance

--- run.py
+++ run.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI, Query
 import uvicorn
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fonts.SimSun.SimSun32x32 import font as SimSun32x32
 
 
 app = FastAPI()
+
+# 允许所有主机
+app.add_middleware(
+    TrustedHostMiddleware, allowed_hosts=["*"]
+)
+
+
+
 
 fonts = {
     "SimSun32x32": SimSun32x32,
@@ -44,7 +53,7 @@ def get_char_size(font):
 # color: 文字颜色，十进制数字，颜色格式为 RGB565，例如蓝色为 31，白色为 65535
 # background: 背景颜色，同上
 @app.get("/bytes")
-def get_bytes_array(font: str = Query, text: str = Query(...), color: int = Query(65535), background: int = Query(0)):
+def get_bytes(font: str = Query, text: str = Query(...), color: int = Query(65535), background: int = Query(0)):
     font = fonts[font]
     # 获得字体中字符大小
     char_width, char_height = get_char_size(font)
@@ -69,14 +78,16 @@ def get_bytes_array(font: str = Query, text: str = Query(...), color: int = Quer
         for char in row_data_str:
             pixel_color = color if char == '1' else background
             # 将单个像素点的颜色（RGB565）转换为两个数字（256进制）存入列表
-            # 方便后续转换为两个字节
-            bytes_list.append(pixel_color // 256)
+            # 将高字节添加到列表中
+            bytes_list.append((pixel_color >> 8) & 0xFF)
+            # 将低字节添加到列表中
+            bytes_list.append(pixel_color & 0xFF)
 
     return {
         "char_width": char_width,
         "char_height": char_height,
         "char_num": char_num,
-        "bytes": bytes_list
+        "byte_list": bytes_list
     }
 
 
